84a824e0d9047b6a4cd5441e3a82f55774e16009
Matthew Browning <mjbrowningg@gmail.com>
2022-12-01 08:21:52 -0800

    work

--- layouts/DefaultLayout/index.tsx
+++ layouts/DefaultLayout/index.tsx
@@ -13,7 +13,7 @@ export const DefaultLayout = ({children}) => {
 const DefaultHeader = () => {
     return (
         <Header height={65}>
-            <Container mx={"auto"} h={"100%"}>
+            <Container size={"md"} mx={"auto"} h={"100%"}>
                 <Group align={"center"} position={"apart"} sx={{height: "100%"}}>
                     <Anchor component={NextLink} href={"/"} size={25} sx={{fontFamily: "monospace"}}>SiCa</Anchor>
                     <ActionIcon component={NextLink} href={"https://github.com/MtthwBrwng/silent-cartographer"} color={"grape"}><BsGithub color={"inherit"} size={25}/></ActionIcon>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import {Anchor, Button, Center, Container, Group, Loader, Menu, SimpleGrid, Stack, Text, Title} from "@mantine/core";
+import {Anchor, Center, Chip, Container, Divider, Group, Loader, SimpleGrid, Stack, Text, Title} from "@mantine/core";
 import {TagOperator, useListTransactionsQuery} from "../graphql/generated";
 import React, {useState} from "react";
 import {DefaultLayout} from "../layouts/DefaultLayout";
-import Divider = Menu.Divider;
 
 export default function Home() {
-    const [nextCursor, setNextCursor] = useState<string | undefined>()
+    const [deviceRecordTypeFilter, setDeviceRecordTypeFilter] = useState<string[] | undefined>(["Device-Create", "Device-Media", "Device-Event"])
+
+
     const {status, data, error, isLoading, isRefetching} = useListTransactionsQuery({
         tagFilter: [
             {name: "App-Name", values: ["ERS"]},
             {
                 name: "Device-Record-Type",
-                values: ["Device-Create"]
+                values: deviceRecordTypeFilter
             },
             {name: "Device-Id", op: TagOperator.Neq, values: [""]},
         ],
         first: 25,
-        after: nextCursor,
-    }, {keepPreviousData: true, queryKey: ["chips", nextCursor],})
+    }, {keepPreviousData: true})
 
     if (isLoading && !isRefetching) {
         return (
@@ -29,53 +29,88 @@ export default function Home() {
 
     const edges = data?.transactions?.edges
     const cursor = edges[edges.length - 1]?.cursor
-// "Device-Create",  "Device-Media", "Device-Event"
+
     return (
-        <Container>
+        <Container size={"md"}>
             <Stack spacing={25}>
-                <Group align={"center"} position={"apart"}>
-                    <Title sx={{fontFamily: "monospace"}}>SiLo Chips:</Title>
-                    <Button variant={"outline"} size={"xs"} radius={"sm"} onClick={() => setNextCursor(cursor)} loading={isRefetching}>Next</Button>
-                </Group>
-                {edges?.map((edge, i) => {
-                    const node = edge.node
+                <Title sx={{fontFamily: "monospace"}}>SiLo Chips</Title>
+                <Chip.Group position={"apart"} value={deviceRecordTypeFilter} onChange={(value => setDeviceRecordTypeFilter(value))} multiple>
+                    <Chip size={"lg"} value="Device-Create">Device Create</Chip>
+                    <Chip size={"lg"} value="Device-Media">Device Media</Chip>
+                    <Chip size={"lg"} value="Device-Event">Device Event</Chip>
+                </Chip.Group>
+                <Stack spacing={25}>
+                    {edges?.map((edge, i) => {
+                        const node = edge.node
 
-                    const meta: { name: string, description: string } = JSON.parse(safeTag(node, "Device-Token-Metadata", null))
-                    const ipfsCid = safeTag(node, "IPFS-Add", null)
+                        const meta: { name: string, description: string } = JSON.parse(safeTag(node, "Device-Token-Metadata", null))
+                        const ipfsCid = safeTag(node, "IPFS-Add", null)
 
-                    return (
-                        <Stack key={`Edge-${i}`}>
-                            <SimpleGrid cols={2}>
-                                <Group sx={{fontFamily: "monospace"}} noWrap>
-                                    <Text color={"dimmed"} inherit>Device Id:</Text>
-                                    <Text inherit>{truncate(safeTag(node, "Device-Id", null), 40)}</Text>
-                                </Group>
-                                <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
-                                    <Text color={"dimmed"} inherit>Device Address:</Text>
-                                    <Anchor
-                                        href={`https://etherscan.io/address/${safeTag(node, "Device-Address", null)}`}
-                                        inherit>{truncate(safeTag(node, "Device-Address", null) ?? "--", 24)}</Anchor>
+                        const deviceRecordType = safeTag(node, "Device-Record-Type", null)
 
-                                </Group>
-                                <Group sx={{fontFamily: "monospace"}} noWrap>
-                                    <Text color={"dimmed"} inherit>Device Manufacturer:</Text>
-                                    <Text
-                                        inherit>{truncate(safeTag(node, "Device-Manufacturer", null) ?? "--", 24)}</Text>
-                                </Group>
-                                <Group sx={{fontFamily: "monospace"}} noWrap>
-                                    <Text color={"dimmed"} inherit>Device Model:</Text>
-                                    <Text inherit>{truncate(safeTag(node, "Device-Model", null) ?? "--", 24)}</Text>
-                                </Group>
-                                <Group sx={{fontFamily: "monospace"}} noWrap>
-                                    <Text color={"dimmed"} inherit>Device Record Type:</Text>
-                                    <Text inherit>{safeTag(node, "Device-Record-Type", null)}</Text>
-                                </Group>
-                            </SimpleGrid>
-                            <Divider/>
-                        </Stack>
-                    )
-                })}
+                        return (
+                            <Stack key={`Edge-${i}`}>
+                                {deviceRecordType === "Device-Create" && (
+                                    <SimpleGrid cols={2}>
+                                        <Group sx={{fontFamily: "monospace"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Id:</Text>
+                                            <Text inherit>{truncate(safeTag(node, "Device-Id", null), 24)}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Address:</Text>
+                                            <Anchor href={`https://etherscan.io/address/${safeTag(node, "Device-Address", null)}`} inherit>{truncate(safeTag(node, "Device-Address", null) ?? "--", 24)}</Anchor>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Manufacturer:</Text>
+                                            <Text inherit>{truncate(safeTag(node, "Device-Manufacturer", null) ?? "--", 24)}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Model:</Text>
+                                            <Text inherit>{truncate(safeTag(node, "Device-Model", null) ?? "--", 24)}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Record Type:</Text>
+                                            <Text inherit>{safeTag(node, "Device-Record-Type", null)}</Text>
+                                        </Group>
+                                    </SimpleGrid>
+                                )}
+                                {deviceRecordType === "Device-Media" && (
+                                    <SimpleGrid cols={2}>
+                                        <Group sx={{fontFamily: "monospace"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Id:</Text>
+                                            <Text inherit>{truncate(safeTag(node, "Device-Id", null), 24)}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Content Type:</Text>
+                                            <Text inherit>{safeTag(node, "Content-Type", null)}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Name:</Text>
+                                            <Text inherit>{meta?.name}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Description:</Text>
+                                            <Text lineClamp={1} inherit>{meta?.description !== "" ? meta?.description : "--"}</Text>
+                                        </Group>
+                                        <Group sx={{fontFamily: "monospace", whiteSpace: "nowrap"}} noWrap>
+                                            <Text color={"dimmed"} inherit>Device Minter:</Text>
+                                            <Anchor href={`https://etherscan.io/address/${safeTag(node, "Device-Minter", null)}`} inherit>{truncate(safeTag(node, "Device-Minter", null) ?? "--", 24)}</Anchor>
+                                        </Group>
+                                    </SimpleGrid>
+                                )}
+                                {deviceRecordType === "Device-Event" && (
+                                    <SimpleGrid cols={2}>
+
+                                    </SimpleGrid>
+                                )}
+                                <Divider/>
+                            </Stack>
+                        )
+                    })}
+                </Stack>
             </Stack>
+
+
         </Container>
     )
 }
